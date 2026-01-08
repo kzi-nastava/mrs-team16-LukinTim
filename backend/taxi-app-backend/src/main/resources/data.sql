70bfe6f95af711af65d0101dbd1124500e3ea6ee
Luka Nikolic <lukanikolic98@hotmail.com>
2026-01-08 19:02:58 +0100

    Implemented login feature for users

---
+++ backend/taxi-app-backend/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (email, firstname, lastname, password, role, activated, address, phonenumber)
+VALUES ('lukadroid98@gmail.com', 'Luka', 'Nikolic', '$2a$10$oPcCRrXY14HPe.PhK5VsQOXecQ68unnOzo5FA6hqgQkpIXktzcD3O', 'ROLE_REGULAR', true, 'Adress1', '0657435435');
+
