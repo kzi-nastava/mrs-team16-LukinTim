0ade9b45ff765d3d75e9f6c6d0af54cf6b11390a
Luka Nikolic <lukanikolic98@hotmail.com>
2026-01-08 14:01:44 +0100

    Initiaized front and back end

---
+++ backend/taxi-app-backend/src/main/java/com/luka/taxi_app_backend/TaxiAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.luka.taxi_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxiAppBackendApplication.class, args);
+	}
+
+}
---
+++ backend/taxi-app-backend/src/test/java/com/luka/taxi_app_backend/TaxiAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luka.taxi_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
